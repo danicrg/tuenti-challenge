94032409afdc0de30807cb5dd66fe270ff56854d
Daniel Carlander <carlander.daniel@gmail.com>
2019-02-11 10:10:45 +0100

    waffle clean

--- 1_waffle_love_COMPLETE/waffle.py
+++ 1_waffle_love_COMPLETE/waffle.py
@@ -3,29 +3,20 @@
 import sys
 
 
-def get_arguments(filename):
-    f = open(filename, 'r')
-    lines = f.readlines()
-    waffles = []
-    for waffle in lines[1:]:
-        waffle = waffle.replace('\n', '').split(' ')
-        waffles.append(waffle)
-    return waffles
-
-
 def main():
-    f = open(sys.argv[2], 'w')
-    counter = 0
-    for waffle in waffles:
-        counter += 1
+    f = open(sys.argv[1], 'r')
+    output = open(sys.argv[2], 'w')
+    cases = int(f.readline())
+
+    for case in range(1, cases + 1):
+        waffle = f.readline().rstrip().split(' ')
         holes = (int(waffle[0]) - 1) * (int(waffle[1]) - 1)
-        f.write(f'Case #{counter}: {holes}\n')
-    f.close()
+        output.write(f'Case #{case}: {holes}\n')
+    output.close()
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         print('Usage: waffles input.in output.out')
     else:
-        waffles = get_arguments(sys.argv[1])
         main()
